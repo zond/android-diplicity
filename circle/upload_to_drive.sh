1a8513920cc7ae627944a6ef7b00a11359d7bc31
Martin Bruse <zond@google.com>
2016-12-17 15:04:17 +0100

    Added android version code to the build file name.

--- circle/upload_to_drive.sh
+++ circle/upload_to_drive.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-APK_NAME="build-${CIRCLE_BUILD_NUM}-`git rev-parse --short HEAD`.apk"
+APK_NAME="build-${CIRCLE_BUILD_NUM}-`git log | grep '^commit' | wc -l`-`git rev-parse --short HEAD`.apk"
 
 $HOME/.go_workspace/bin/gdrive --refresh-token ${GDRIVE_REFRESH_TOKEN} upload -p ${GDRIVE_PARENT_FOLDER} --name ${APK_NAME} app/build/outputs/apk/app-release.apk
