7f6b7f414c6eef18d00c8de706b9716488c68489
Martin Bruse <zond@google.com>
2016-12-18 13:17:04 +0100

    Better versioning.

--- circle/upload_to_drive.sh
+++ circle/upload_to_drive.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-APK_NAME="build-${CIRCLE_BUILD_NUM}-`git log | grep '^commit' | wc -l`-`git rev-parse --short HEAD`.apk"
+APK_PATH="app/build/outputs/apk/app-release.apk"
+APK_VERSION_LINE=`/usr/local/android-sdk-linux/build-tools/25.0.1/aapt dump badging ${APK_PATH}`
+APK_VERSION_CODE=`echo ${APK_VERSION_LINE} | sed -e "s/.*versionCode='\([^']\+\)'.*/\1/"`
+APK_VERSION_NAME=`echo ${APK_VERSION_LINE} | sed -e "s/.*versionName='\([^']\+\)'.*/\1/"`
+APK_NAME="build-${CIRCLE_BUILD_NUM}-${APK_VERSION_CODE}-${APK_VERSION_NAME}"
 
-$HOME/.go_workspace/bin/gdrive --refresh-token ${GDRIVE_REFRESH_TOKEN} upload -p ${GDRIVE_PARENT_FOLDER} --name ${APK_NAME} app/build/outputs/apk/app-release.apk
+$HOME/.go_workspace/bin/gdrive --refresh-token ${GDRIVE_REFRESH_TOKEN} upload -p ${GDRIVE_PARENT_FOLDER} --name ${APK_NAME} ${APK_PATH}
