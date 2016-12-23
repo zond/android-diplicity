dca218d795cd0b0ed76788a1839c5cc58343f0b0
Martin Bruse <zond@google.com>
2016-12-23 03:06:31 +0100

    Protected against a null pointer exception when validating game phase length.

--- app/src/main/java/se/oort/diplicity/MainActivity.java
+++ app/src/main/java/se/oort/diplicity/MainActivity.java
@@ -92,7 +92,7 @@ public class MainActivity extends RetrofitActivity {
                         final Returner<Game, Boolean> validateGame = new Returner<Game, Boolean>() {
                             @Override
                             public Boolean Return(Game g) {
-                                if (g.PhaseLengthMinutes > 30 * 24 * 60) {
+                                if (g.PhaseLengthMinutes != null && g.PhaseLengthMinutes > 30 * 24 * 60) {
                                     Toast.makeText(MainActivity.this, R.string.phase_length_must_be_less_than_30_days, Toast.LENGTH_LONG).show();
                                     return false;
                                 }
