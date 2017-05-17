c2050ac1fc34971123cde7d90094b1443c6b75ed
Martin Bruse <zondolfin@gmail.com>
2017-05-17 07:51:15 +0200

    More debugging of the build script.

--- circle/create_release.sh
+++ circle/create_release.sh
@@ -5,5 +5,9 @@ APK_VERSION_LINE=`/usr/local/android-sdk-linux/build-tools/25.0.2/aapt dump badg
 APK_VERSION_CODE=`echo ${APK_VERSION_LINE} | sed -e "s/.*versionCode='\([^']\+\)'.*/\1/"`
 APK_VERSION_NAME=`echo ${APK_VERSION_LINE} | sed -e "s/.*versionName='\([^']\+\)'.*/\1/"`
 
+echo ${APK_VERSION_LINE}
+echo ${APK_VERSION_CODE}
+echo ${APK_VERSION_NAME}
+
 go get github.com/google/go-github/github golang.org/x/oauth2 google.golang.org/api/drive/v3
 go run circle/create_release.go -apk=${APK_PATH} -build=${CIRCLE_BUILD_NUM} -tag=${APK_VERSION_CODE} -short_sha=${APK_VERSION_NAME}
