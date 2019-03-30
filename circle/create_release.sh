bebdd94a0151dab33a0767bc588d7a5c85bd480b
tttppp <tttppp@users.noreply.github.com>
2019-03-30 19:34:42 +0000

    Try installing add-apt-repository.

--- circle/create_release.sh
+++ circle/create_release.sh
@@ -12,7 +12,8 @@ echo ${APK_VERSION_LINE}
 echo ${APK_VERSION_CODE}
 echo ${APK_VERSION_NAME}
 
-sudo add-apt-repository ppa:gophers/archive
+sudo apt-get install software-properties-common
+sudo add-apt-repository -y ppa:gophers/archive
 sudo apt update
 sudo apt-get install golang-1.9-go
 export GOPATH="${HOME}/go"
