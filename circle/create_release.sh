151b515d8d4b6471b11faa9cc8c45fe283ce12a7
tttppp <tttppp@users.noreply.github.com>
2019-03-30 19:28:52 +0000

    Try using another repository for Go 1.9.

--- circle/create_release.sh
+++ circle/create_release.sh
@@ -12,7 +12,9 @@ echo ${APK_VERSION_LINE}
 echo ${APK_VERSION_CODE}
 echo ${APK_VERSION_NAME}
 
-sudo apt-get install golang-1.9
+sudo add-apt-repository ppa:gophers/archive
+sudo apt update
+sudo apt-get install golang-1.9-go
 export GOPATH="${HOME}/go"
-/usr/lib/go-1.8/bin/go get github.com/google/go-github/github golang.org/x/oauth2 google.golang.org/api/drive/v3
-/usr/lib/go-1.8/bin/go run circle/create_release.go -apk=${APK_PATH} -build=${CIRCLE_BUILD_NUM} -tag=${APK_VERSION_CODE} -short_sha=${APK_VERSION_NAME}
+/usr/lib/go-1.9/bin/go get github.com/google/go-github/github golang.org/x/oauth2 google.golang.org/api/drive/v3
+/usr/lib/go-1.9/bin/go run circle/create_release.go -apk=${APK_PATH} -build=${CIRCLE_BUILD_NUM} -tag=${APK_VERSION_CODE} -short_sha=${APK_VERSION_NAME}
