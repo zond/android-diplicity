9ab8c759b2ccdcb992206c492c426cdd8272b839
Martin Bruse <zondolfin@gmail.com>
2020-03-05 16:11:12 +0100

    More GitHub actions work.

--- tools/create_release.sh
+++ tools/create_release.sh
@@ -4,9 +4,11 @@ echo 'Will create release...'
 pwd
 
 APK_PATH="app/build/outputs/apk/release/app-release.apk"
-APK_VERSION_LINE=`/opt/android/sdk/build-tools/27.0.3/aapt dump badging ${APK_PATH}`
-APK_VERSION_CODE=`echo ${APK_VERSION_LINE} | sed -e "s/.*versionCode='\([^']\+\)'.*/\1/"`
-APK_VERSION_NAME=`echo ${APK_VERSION_LINE} | sed -e "s/.*versionName='\([^']\+\)'.*/\1/"`
+AAPT_PATH="(find "${ANDROID_HOME}" -name aapt | head)"
+echo ${AAPT_PATH}
+APK_VERSION_LINE=$(${AAPT_PATH} dump badging ${APK_PATH})
+APK_VERSION_CODE=$(echo ${APK_VERSION_LINE} | sed -e "s/.*versionCode='\([^']\+\)'.*/\1/")
+APK_VERSION_NAME=$(echo ${APK_VERSION_LINE} | sed -e "s/.*versionName='\([^']\+\)'.*/\1/")
 
 echo ${APK_VERSION_LINE}
 echo ${APK_VERSION_CODE}
