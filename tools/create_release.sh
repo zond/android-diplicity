b5d90f0c1d5b814f558d34501d4a16d499f5b2d3
Martin Bruse <zondolfin@gmail.com>
2020-03-05 16:52:53 +0100

    More GitHub actions work.

--- tools/create_release.sh
+++ tools/create_release.sh
@@ -10,9 +10,9 @@ APK_VERSION_LINE=$(${AAPT_PATH} dump badging ${APK_PATH})
 APK_VERSION_CODE=$(echo ${APK_VERSION_LINE} | sed -e "s/.*versionCode='\([^']\+\)'.*/\1/")
 APK_VERSION_NAME=$(echo ${APK_VERSION_LINE} | sed -e "s/.*versionName='\([^']\+\)'.*/\1/")
 
-echo ${APK_VERSION_LINE}
 echo ${APK_VERSION_CODE}
 echo ${APK_VERSION_NAME}
 
 go version
-go run tools/create_release.go -apk=${APK_PATH} -build=${CIRCLE_BUILD_NUM} -tag=${APK_VERSION_CODE} -short_sha=${APK_VERSION_NAME}
+cd tools
+go run create_release.go -apk=${APK_PATH} -build=${CIRCLE_BUILD_NUM} -tag=${APK_VERSION_CODE} -short_sha=${APK_VERSION_NAME}
