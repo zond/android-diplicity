f43b6e4891500acdb5e62e4b2825d8e54fcf5414
Martin Bruse <zondolfin@gmail.com>
2020-03-05 15:07:43 +0100

    More GitHub actions work.

--- tools/download_keystore.sh
+++ tools/download_keystore.sh
@@ -4,7 +4,7 @@ if [ "${KEYSTORE_URL}" != "" ]; then
     echo "KEYSTORE_URL found - downloading..."
     # we're using curl instead of wget because it will not
     # expose the sensitive uri in the build logs:
-    curl -L -o "${GITHUB_WORKSPACE}/android-diplicity/signing.keystore" "${KEYSTORE_URL}"
+    curl -L -o "${GITHUB_WORKSPACE}/signing.keystore" "${KEYSTORE_URL}"
 else
     echo "KEYSTORE_URL not set. APK artifact will not be signed."
 fi
