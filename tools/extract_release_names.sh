ae125277662338defcaa4ae5dfaaa0e4921c4d53
Martin Bruse <zondolfin@gmail.com>
2020-03-05 17:13:36 +0100

    More GitHub actions work.

--- tools/extract_release_names.sh
+++ tools/extract_release_names.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-echo 'Will create release...'
-pwd
 
 APK_PATH="app/build/outputs/apk/release/app-release.apk"
 AAPT_PATH="$(ls -1 $ANDROID_HOME/build-tools/*/aapt|tail -n 1)"
-echo ${AAPT_PATH}
+echo "Extracting release names from ${APK_PATH} using ${AAPT_PATH}."
+
 APK_VERSION_LINE=$(${AAPT_PATH} dump badging ${APK_PATH})
 APK_VERSION_CODE=$(echo ${APK_VERSION_LINE} | sed -e "s/.*versionCode='\([^']\+\)'.*/\1/")
 APK_VERSION_NAME=$(echo ${APK_VERSION_LINE} | sed -e "s/.*versionName='\([^']\+\)'.*/\1/")
+
+echo "Found version code ${APK_VERSION_CODE}."
+echo "Found version name ${APK_VERSION_NAME}."
 
 echo ${APK_VERSION_CODE} > ${GITHUB_WORKSPACE}/versionCode.txt
 echo ${APK_VERSION_NAME} > ${GITHUB_WORKSPACE}/versionName.txt
